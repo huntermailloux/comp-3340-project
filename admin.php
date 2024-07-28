794254ff005f1dc59da57536780aae6648c97527
Edward Nafornita <eddie.nafornita@gmail.com>
2024-07-27 23:59:02 -0400

    admin.php - main administrative dashboard. admin_delete_user/post.php used scripts that are used to delete users/posts from the database using prepared SQL statements. fetch_comms/posts/users.php - script that grabs comms/posts/users records from their associated tables from the database using prepared SQL statements. search.js - implements the search bar that allows admins to search for a specific username and populates the posts table with all the posts submitted by that user. search.php - php script that grabs the data to display the result of a query on the posts table (done using prepared SQL statements). postsTable.css/tables.css - just the table styles. tables.js - the main javascript file that fetches data from the database, displays the data and creates the associated pagination for each table.

--- admin.php
+++ admin.php
@@ -1,9 +1,3 @@
-<!-- TODO: -->
-<!-- Persistent admin session -->
-<!-- Section to query the Posts table -->
-<!--    be able to display all comments under said Post -->
-<!-- View User Communications -->
-
 <?php
     require 'connectionString.php';
     $userId = $_SESSION['userId'];
@@ -18,24 +12,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Create Tables</title>
+    <title>Admin Dashboard</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>
-        document.write("<script type='text/javascript' src='databaseHandler.js?v=" + Date.now() + "'><\/script>");
+        document.write("<link rel='stylesheet' href='tables.css?v=" + Date.now() + "'><\/link>");
     </script>
     <script>
-        document.write("<link rel='stylesheet' href='table.css?v=" + Date.now() + "'><\/link>");
+        document.write("<script type='text/javascript' src='tables.js?v=" + Date.now() + "'><\/script>");
     </script>
     <script>
-        document.write("<script type='text/javascript' src='table.js?v=" + Date.now() + "'><\/script>");
+        document.write("<script type='text/javascript' src='search.js?v=" + Date.now() + "'><\/script>");
+    </script>
+    <script>
+        document.write("<link rel='stylesheet' href='postsTable.css?v=" + Date.now() + "'><\/link>");
     </script>
 </head>
 <body>
-    <h1>Create Database Tables</h1>
-    <button onclick="createTables()">Create tables</button>
-
-    <div id="table-container">
+    <div class="table-container">
+        <h1>Table of All Users</h1>
         <table id="classTable">
             <thead>
                 <tr>
@@ -43,13 +38,57 @@
                     <th>Username</th>
                     <th>First Name</th>
                     <th>Last Name</th>
+                    <th>Options</th>
                 </tr>
             </thead>
             <tbody>
                 
             </tbody>
         </table>
+        <div class="pagination" id="userPagination"></div>
     </div>
-    <div class="pagination" id="pagination"></div>
+
+    <div class="table-container">
+        <h1>Table of All Posts</h1>
+        <div class="search-container">
+            <form action="search.php" id="searchForm" method="GET">
+                <input type="text" id="searchQuery" name="query" placeholder="Search posts...">
+                <button type="submit">Search</button>
+            </form>   
+        </div>
+        <table id="classTable2">
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>Title</th>
+                    <th>Content</th>
+                    <th>Timestamp</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+        
+            </tbody>
+        </table>
+        <div class="pagination" id="postsPagination"></div>
+    </div>
+
+    <div class="table-container">
+        <h1>Table of User Submitted Contacts</h1>
+        <table id="classTable3">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                    <th>Timestamp</th>
+                </tr>
+            </thead>
+            <tbody>
+        
+            </tbody>
+        </table>
+        <div class="pagination" id="commsPagination"></div>
+    </div>
 </body>
 </html>
